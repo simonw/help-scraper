60cb270f5b6e03afbd130af084bfbd2ef145079d
Simon Willison <swillison@gmail.com>
2022-01-29 17:42:46 +0000

    Handle some edge-cases, refs #2

--- aws_commands.py
+++ aws_commands.py
@@ -3,18 +3,19 @@ import re
 import subprocess
 
 
-backspace_re = re.compile(r'(.)\x08')
+backspace_re = re.compile(r"(.)\x08")
+
 
 def apply_backspaces(s):
-    return backspace_re.sub('', s)
+    return backspace_re.sub("", s)
 
 
-def extract_commands(s):
-    if "AVAILABLE " not in s:
+def extract_commands(s, command_heading="AVAILABLE COMMANDS"):
+    if command_heading not in s:
         return []
-    lines = s.split("AVAILABLE ")[1].split("SEE ALSO")[0].strip().split('\n')
-    commands = [c.split(' o ')[1].strip() for c in lines if ' o ' in c]
-    return [c for c in commands if c != "help"]
+    lines = s.split(command_heading)[1].split("SEE ALSO")[0].strip().split("\n")
+    commands = [c.split(" o ")[1].strip() for c in lines if " o " in c]
+    return [c for c in commands if c != "help" and " " not in c]
 
 
 def fetch_and_save(bits):
@@ -28,7 +29,10 @@ def fetch_and_save(bits):
         filename = bits[-1] + ".txt"
         dir = pathlib.Path("/".join(bits[:-1]))
     dir.mkdir(parents=True, exist_ok=True)
-    s = subprocess.run(list(bits) + ["help"], capture_output=True).stdout.decode("utf-8")
+    proc = subprocess.run(list(bits) + ["help"], capture_output=True)
+    if proc.returncode != 0:
+        return None
+    s = proc.stdout.decode("utf-8")
     s = apply_backspaces(s)
     (dir / filename).write_text(s, "utf-8")
     return s
@@ -36,15 +40,22 @@ def fetch_and_save(bits):
 
 if __name__ == "__main__":
     # Start with aws help
-    to_do = [("aws",)]
+    to_do = [("aws", "fsx")]
     done = set()
     while to_do:
         next_bits = to_do.pop()
         print(next_bits)
         s = fetch_and_save(next_bits)
         done.add(next_bits)
+        if s is None:
+            continue
         # Extract commands and add them to the list
-        for command in extract_commands(s):
+        for command in extract_commands(
+            s,
+            command_heading=(
+                "AVAILABLE SERVICES" if next_bits == ("aws",) else "AVAILABLE COMMANDS"
+            ),
+        ):
             path = tuple(list(next_bits) + [command])
             if path not in done:
                 to_do.append(path)
