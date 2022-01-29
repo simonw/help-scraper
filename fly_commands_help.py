7b7a91db5a2acb3908b994c9203c16297488be49
Simon Willison <swillison@gmail.com>
2022-01-29 00:12:15 +0000

    Scraper for Fly --help

---
+++ fly_commands_help.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    s = open("flyctl/help.txt").read()
+    lines = s.split("Commands:")[1].split("\n\nFlags:")[0].strip().split("\n")
+    commands = [l.strip().split()[0] for l in lines]
+    for command in commands:
+        print(
+            "~/.fly/bin/flyctl {command} --help > flyctl/{command}.txt".format(
+                command=command
+            )
+        )
