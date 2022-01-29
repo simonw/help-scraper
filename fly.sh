498383396e5bbba413308b2877ea75caa67132c5
Simon Willison <swillison@gmail.com>
2022-01-28 17:44:53 -0800

    Also store flyctl version

--- fly.sh
+++ fly.sh
@@ -2,4 +2,5 @@
 curl -L https://fly.io/install.sh | sh
 mkdir -p flyctl
 ~/.fly/bin/flyctl --help > flyctl/flyctl-help.txt
-python fly_commands_help.py  | sh
+~/.fly/bin/flyctl version > flyctl/flyctl-flyctl-version.txt
+python fly_commands_help.py | sh
