be4fe6bebf76ae6e2a4dbac2dfdfbccc8ab8ef46
Simon Willison <swillison@gmail.com>
2022-03-19 16:40:45 -0700

    Include SQLite version
    Refs #12

--- sqlite3_help.py
+++ sqlite3_help.py
@@ -2,15 +2,25 @@ import subprocess
 
 
 if __name__ == "__main__":
-    proc = subprocess.run("sqlite3", capture_output=True, input=b".help")
-    help = proc.stdout.decode("utf-8")
+    help = subprocess.run("sqlite3", capture_output=True, input=b".help").stdout.decode(
+        "utf-8"
+    )
+    # Get the version
+    version = (
+        subprocess.run(["sqlite3", "--version"], capture_output=True)
+        .stdout.decode("utf-8")
+        .strip()
+    )
     commands = [line.split()[0] for line in help.split("\n") if line.startswith(".")]
     details = []
     for command in commands:
         filename = command.lstrip(".") + ".txt"
-        proc2 = subprocess.run("sqlite3", capture_output=True, input=".help {}".format(command).encode("utf-8"))
-        command_help = proc2.stdout.decode("utf-8")
+        command_help = subprocess.run(
+            "sqlite3",
+            capture_output=True,
+            input=".help {}".format(command).encode("utf-8"),
+        ).stdout.decode("utf-8")
         details.append(command_help.strip())
-    print("# sqlite3 .help\n```")
+    print("# sqlite3 .help\n\nVersion: `{}`\n\n```".format(version))
     print("\n\n".join(details))
     print("```")
