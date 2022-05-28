433bf2a06e8ef2029dd56950aaeb5d191a083518
Simon Willison <swillison@gmail.com>
2022-05-28 08:00:20 -0700

    Capture fly subcommands, closes #1

--- fly.sh
+++ fly.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 curl -L https://fly.io/install.sh | sh
 mkdir -p flyctl
-~/.fly/bin/flyctl --help > flyctl/flyctl-help.txt
-~/.fly/bin/flyctl version > flyctl/flyctl-flyctl-version.txt
-python fly_commands_help.py | sh
+python fly_commands_help.py ~/.fly/bin/flyctl
--- fly_commands_help.py
+++ fly_commands_help.py
@@ -1,10 +1,33 @@
+import subprocess
+import sys
+
+run = subprocess.check_output
+
+def extract_commands(output):
+    try:
+        lines = output.split("Commands:")[1].split("\n\nFlags:")[0].strip().split("\n")
+    except IndexError:
+        return []
+    return [l.strip().split()[0] for l in lines if l.strip()]
+
+
 if __name__ == "__main__":
-    s = open("flyctl/flyctl-help.txt").read()
-    lines = s.split("Commands:")[1].split("\n\nFlags:")[0].strip().split("\n")
-    commands = [l.strip().split()[0] for l in lines]
+    fly_bin = sys.argv[-1]
+    assert fly_bin.endswith("flyctl")
+
+    def run(*args):
+        return subprocess.check_output([fly_bin] + list(args))
+
+    open("flyctl/flyctl-flyctl-version.txt", "w").write(run("version"))
+    help = run("--help")
+    open("flyctl/flyctl-help.txt", "w").write(help)
+
+    commands = extract_commands(help)
     for command in commands:
-        print(
-            "~/.fly/bin/flyctl {command} --help > flyctl/{command}.txt".format(
-                command=command
-            )
-        )
+        command_help = run(command, "--help")
+        open("flyctl/{}.txt".format(command), "w").write(command_help)
+        # Only one nested recursion, I don't think Fly has deeper than that
+        subcommands = extract_commands(command_help)
+        for subcommand in subcommands:
+            subcommand_help = run(command, subcommand, "--help")
+            open("flyctl/{}-{}.txt".format(command, subcommand), "w").write(subcommand_help)
