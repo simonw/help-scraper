b7823622b48fac67f48f76448c496570e710531f
Simon Willison <swillison@gmail.com>
2022-05-28 08:34:12 -0700

    Capture Fly sub-sub-commands too, refs #1

--- fly_commands_help.py
+++ fly_commands_help.py
@@ -6,31 +6,34 @@ run = subprocess.check_output
 
 def extract_commands(output):
     try:
-        lines = output.split("Commands:")[1].split("\n\nFlags:")[0].strip().split("\n")
+        lines = output.split("Commands:")[1].split("\n\nFlags:")[0].split("\n")
     except IndexError:
         return []
-    return [l.strip().split()[0] for l in lines if l.strip()]
+    commands = [l.strip().split()[0] for l in lines if l.strip() and l[:2] == "  "]
+    print(commands)
+    return commands
+
+
+def save_help_for_command_and_subcommands(run, path):
+    print(path)
+    command_help = run(path + ["--help"])
+    open("flyctl/{}.txt".format("-".join(path)), "w").write(command_help)
+    subcommands = extract_commands(command_help)
+    for subcommand in subcommands:
+        save_help_for_command_and_subcommands(run, path + [subcommand])
 
 
 if __name__ == "__main__":
     fly_bin = sys.argv[-1]
     assert fly_bin.endswith("flyctl")
 
-    def run(*args):
-        return subprocess.check_output([fly_bin] + list(args)).decode("latin-1")
+    def run(args):
+        return subprocess.check_output([fly_bin] + args).decode("latin-1")
 
-    open("flyctl/flyctl-flyctl-version.txt", "w").write(run("version"))
-    help = run("--help")
+    open("flyctl/flyctl-flyctl-version.txt", "w").write(run(["version"]))
+    help = run(["--help"])
     open("flyctl/flyctl-help.txt", "w").write(help)
 
     commands = extract_commands(help)
     for command in commands:
-        command_help = run(command, "--help")
-        open("flyctl/{}.txt".format(command), "w").write(command_help)
-        # Only one nested recursion, I don't think Fly has deeper than that
-        subcommands = extract_commands(command_help)
-        for subcommand in subcommands:
-            subcommand_help = run(command, subcommand, "--help")
-            open("flyctl/{}-{}.txt".format(command, subcommand), "w").write(
-                subcommand_help
-            )
+        save_help_for_command_and_subcommands(run, [command])
