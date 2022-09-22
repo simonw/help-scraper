02f20c367ef09e607a13008b01272ce33a7f54fd
Simon Willison <swillison@gmail.com>
2022-09-22 11:01:11 -0700

    Also grab vm-sizes

--- fly.sh
+++ fly.sh
@@ -5,3 +5,5 @@ python fly_commands_help.py ~/.fly/bin/flyctl
 
 # Grab a list of current Fly regions too
 ~/.fly/bin/flyctl platform regions > flyctl/fly-regions.txt
+# And vm-sizes
+~/.fly/bin/flyctl platform vm-sizes > flyctl/fly-vm-sizes.txt
