15e4980c323283bd1c0cc4b465dcd4e13f4843f8
Simon Willison <swillison@gmail.com>
2022-09-22 10:59:38 -0700

    Also save fly-regions

--- fly.sh
+++ fly.sh
@@ -2,3 +2,6 @@
 curl -L https://fly.io/install.sh | sh
 mkdir -p flyctl
 python fly_commands_help.py ~/.fly/bin/flyctl
+
+# Grab a list of current Fly regions too
+~/.fly/bin/flyctl platform regions > flyctl/fly-regions.txt
